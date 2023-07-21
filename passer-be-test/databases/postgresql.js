a74ff0372c8db6cea86fae6eabd8b81e0e041af5
Maito1794 <isaacmmorab@gmail.com>
2023-07-20 20:43:03 -0600

    se crea la tabla transaction

--- passer-be-test/databases/postgresql.js
+++ passer-be-test/databases/postgresql.js
@@ -6,6 +6,7 @@ const postgresql = newDb();
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
                 insert into users values (123, 'Juan', true);`);
 
+postgresql.public.none(`create table transaction(pk_transaction integer, fk_user integer, description text, amount float);`);
 
 module.exports = {
     postgresql
