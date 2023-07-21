cde4939ed5f880ad24548e2debfe809ab22f7971
Maito1794 <isaacmmorab@gmail.com>
2023-07-20 22:13:53 -0600

    se agrega await a la llamada del método del modelo

--- passer-be-test/services/transaction.js
+++ passer-be-test/services/transaction.js
@@ -10,7 +10,7 @@ const transactionModel = require('../models/transaction')
  */
 const createTransaction = async (pk_transaction, fk_user, description, amount) => {
     try {
-        return transactionModel.createTransaction(pk_transaction, fk_user, description, amount)
+        return await transactionModel.createTransaction(pk_transaction, fk_user, description, amount)
     } catch (e) {
         throw new Error(e.message)
     }
